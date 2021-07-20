047935ce74097570c0e41824188482c339b66542
Riyad <riyad298@gmail.com>
2021-07-20 17:56:48 +0600

    pick is ready

--- pick/pick.ts
+++ pick/pick.ts
@@ -1,64 +1,4 @@
-interface map<B> {
-    [ key: string ] : B
-}
-
-function process<T, W>(param: T, obj: W) {
-    const newObj = {...obj};
-    let reducedValue: any = "";
-    if(typeof param == 'string')
-    {
-        reducedValue =  param.split('.').reduce((acc: { [key: string] : any }, curr)=>{
-            return acc ? acc[curr] : null;
-        }, newObj)
-    }
-    return reducedValue;
-}
-
-
-
-function checkType<T , W extends { [key: string] : any }>(param: T , obj: W ) : string{
-    let reducedValue = '';
-    if( typeof param == 'string' ) {
-        reducedValue  =  process(param, obj);
-        return reducedValue;
-    }else{
-        return reducedValue;
-    }
-}
-
-
-
-
-let obj: map<string | object>  = {
-    "age": "20",
-    "riyad": {
-        "name" : "riyad"
-    }
-}
-
-let arr: any[] = [ 'age' ,  { name: 'riyad.name'} , 'else' ]
-
-const pick1 = function<T, W> (arr: T[], obj: W){
-    let newObj: { [key: string]: any } = {};
-    arr.forEach(n => {
-        if (typeof n == 'object') {
-            let value = Object.values(n)[0];
-            const newValue = checkType(value, obj);
-            if (newValue) newObj[Object.keys(n)[0]] = newValue;
-
-        } else if (typeof n == 'string') {
-            const newValue = checkType(n, obj);
-            if (newValue) newObj[n] = newValue;
-        }
-    })
-
-    return newObj;
-}
-
-console.log(pick1(arr, obj))
-
-
-export const pick = function(){
+export const pick = function () {
 
     interface map<B> {
         [key: string]: B
@@ -119,5 +59,3 @@ export const pick = function(){
     return pick1;
 
 }();
-
-
